c28152185456d8b366dcfed0cce2f2467c9fa861
Mahdy Mubasyir <m.mubasyir19@gmail.com>
2025-02-05 22:53:16 +0700

    update: layout button plan card

--- src/components/molecules/PlanCard.tsx
+++ src/components/molecules/PlanCard.tsx
@@ -15,11 +15,11 @@ export default function PlanCard({ title, description, price, unit }: PlanCardPr
       <h1 className='mt-7 text-white font-semibold text-2xl'>
         ${price} <span className='text-sm text-grey-60'>/{unit}</span>
       </h1>
-      <div className='mt-7 grid grid-cols-2 gap-x-3'>
-        <button className='bg-black-08 border border-black-15 text-white py-3 px-5 rounded-md font-semibold text-sm'>
+      <div className='mt-7 flex md:flex-col lg:flex-row gap-3'>
+        <button className='flex-1 bg-black-08 border border-black-15 text-white py-3 px-5 rounded-md font-semibold text-sm'>
           Start Free Trial
         </button>
-        <button className='bg-red-45 text-white py-3 px-5 rounded-md font-semibold text-sm'>Choose Plan</button>
+        <button className='flex-1 bg-red-45 text-white py-3 px-5 rounded-md font-semibold text-sm'>Choose Plan</button>
       </div>
     </div>
   );
