e610b5121e0b979708f97a1c81c43a3a5716e0a2
Mahdy Mubasyir <m.mubasyir19@gmail.com>
2025-02-05 23:10:09 +0700

    new: structure folder for pages

--- src/app/(home)/layout.tsx
+++ src/app/(home)/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from 'next';
-import './globals.css';
+import '../globals.css';
 import { Manrope } from 'next/font/google';
 
 const manrope = Manrope({
---
+++ src/app/(menu)/layout.tsx
@@ -0,0 +1,26 @@
+import type { Metadata } from 'next';
+import '../globals.css';
+import { Manrope } from 'next/font/google';
+
+const manrope = Manrope({
+  weight: ['200', '300', '400', '500', '700', '800'],
+  style: 'normal',
+  subsets: ['latin'],
+});
+
+export const metadata: Metadata = {
+  title: 'StreamVibe',
+  description: 'Stream with the vibe',
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang='en'>
+      <body className={`${manrope.className}`}>{children}</body>
+    </html>
+  );
+}
