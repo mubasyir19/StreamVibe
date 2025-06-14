5700802fd589509f2b549c80134cd48275c26709
Mahdy Mubasyir <m.mubasyir19@gmail.com>
2025-06-14 20:47:16 +0700

    add: mock data film

---
+++ src/services/film.ts
@@ -0,0 +1,31 @@
+export const film = [
+  {
+    title: '',
+    description: '',
+    releasedYear: 2022,
+    thumbnail: '',
+    rating: 5.5,
+    genre: ['Adventure', 'Action'],
+    language: ['English', 'Indonesian', 'Arabic', 'Hindi'],
+    cast: [
+      {
+        name: '',
+        photo: '',
+      },
+    ],
+    director: {
+      name: '',
+      photo: '',
+      origin: '',
+    },
+    music: '',
+    reviews: [
+      {
+        name: '',
+        origin: '',
+        content: '',
+        rating: '',
+      },
+    ],
+  },
+];
